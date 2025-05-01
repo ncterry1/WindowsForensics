b731978c3d2e8725981ddbc8087c7ab13b5c72d0
ncterry1 <95770267+ncterry1@users.noreply.github.com>
2025-05-01 13:09:50 -0400

    Update RunWindowsForensicsProgram.ps1

--- RunWindowsForensicsProgram.ps1
+++ RunWindowsForensicsProgram.ps1
@@ -1,3 +1,40 @@
+<###########################################################################
+# This script, "RunWindowsForensicsProgram.ps1" depends on if "InstallWindowsForensicsModule.ps1" has been run first.
+# If "InstallWindowsForensicsModule.ps1" has been executed, just that first time, you can then execute
+#     "RunWindowsForensicsProgram.ps1" any time after that.
+#
+# From PowerShell 
+#     > .\RunWindowsForensicsProgram.ps1
+#
+# Or right click on "RunWindowsForensicsProgram.ps1" and click:
+#     "Run with PowerShell"
+#
+# ALSO - once "InstallWindowsForensicssModule.ps1" has been run, and WindowsForensicsModule has been 
+# installed in the PowerShell cache, you can then copy the "RunWindowsForensicsProgram - Shortcut" anywhere
+# on the machine. For example, once "WindowsForensicsModule" has been installed, 
+#     Right click on "RunWindowsForensicsProgram - Shortcut"
+#     Click:  "Pin to Taskbar"
+# 
+#     Now if you click on this Taskbar icon, the WindowsForensicsModule will run anytime.
+# 
+# 
+# Within the 'WindowsForensicsModule' directory, the 'RunWindowsForensicsProgram' can be run from 
+# any location on the machine; however, regardless of where it is executed from, 
+# the default location that the 'WindowsForensicsProgram' will always use for data, reports, 
+# backups etc., is the 'WindowsPowerShell' location that the program copies 
+# itself to during execution:
+# 
+#   'C:\Program Files\WindowsPowerShell\Modules\WindowsForensicsModule'
+# ###########################################################################
+#
+#
+# This WindowsForensicsprogram is directly tied to the WindowsForensicsModule
+# Inside the WindowsForensicsModule directory is the "WindowsForensicsModule.psm1"
+# This only works once the WindowsForensicsModule has been placed=copied=installed into a PowerShell Directory
+# If the Copy-Item above does not work, such as user has no admin privs, it cant import then.
+# Import uses the instructions on that local "WindowsForensicsModule.psm1"
+#>
+
 Import-Module WindowsForensicsModule -Force
 
 function Display-MainMenu {
@@ -7,27 +44,396 @@ function Display-MainMenu {
 |======================================================================|") # END WRITE-HOST
     #
     Write-Host("`n Selections:
-------------------------------------------------------------------------`n")
+-----------------------------------------------------------------------`n")
     Write-Host -ForegroundColor Yellow " 1) Event Log Analysis"
     Write-Host -ForegroundColor Yellow " 2) Registry Analysis"
     Write-Host -ForegroundColor Yellow " 3) Network Analysis"
-    Write-Host -ForegroundColor Yellow " h) Help | q) Quit"
-}
+    #
 
-do {
+    Write-Host("------------------------------------------------------------------------")
+    Write-Host -ForegroundColor Yellow ("`n   README - Program Summary `t`t`t`tEnter: `'readme`'`n")
+    Write-Host("------------------------------------------------------------------------")
+    Write-Host -ForegroundColor Yellow ("`n   HELP   - Enter the number + h  (1h, 2h, 3h...) `t`tEnter: `'#h`'`n")
+    Write-Host -ForegroundColor Red ("   QUIT   - End the Windows Forensics Program `t`t`tEnter: `'quit`'`n") 
+    Write-Host("------------------------------------------------------------------------")
+} # End Function Display-MainMenu
+#========================================================================
+#--------------------------
+#--------------------------
+# Continuing do loop unless user quits
+# Other menus/functions will resort back here when they are completed.
+$Global:quit = ""   # Var allows for a full session quit from sub menus.
+do
+{
+    If ($Global:quit -eq "quit") {break}
+    Write-Host("`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n") # The screen can leave residual text. This overwrites before clear
+    Clear-Host        # Clear terminal 
+    Display-MainMenu      # Call local function. Print menu to screen
+    $choice = Read-Host "`nEnter Choice"  #Ask user for menu choice
+    Write-Host("`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n") # The screen can leave residual text. This overwrites before clear
     Clear-Host
-    Display-MainMenu
-    $choice = (Read-Host "Enter Selection").Trim().ToLower()
+    $choice = $choice -replace '\s', ''   #In case user enters spaces
+    $choice = $choice.ToLower()           #In case user uses any capitol letters.
 
-    switch ($choice) {
-        '1' { .\Menus\EventLogMenu.ps1 }
-        '2' { .\Menus\RegistryMenu.ps1 }
-        '3' { .\Menus\NetworkMenu.ps1 }
-        'h' { Get-Help WindowsForensicsProgram }
-        'q' { break }
-        default {
-            Write-Warning "Invalid choice '$choice'. Please select again."
-            Start-Sleep -Seconds 2
-        }
-    }
-} while ($true)
+    switch ($choice)  # If menu options are chosen correctly.
+    {
+        # Option 1 - System\Domain Information-------------
+        '1' 
+        {
+            Write-Host "1) You chose To view the System\Domain Information"
+            #.\Menu\1_GPOsDomainMenu.ps1    # Execute GPO Search Menu script.
+            If ($Global:quit -eq "quit") {break}
+            
+
+        }#END option 1-------------------------------------
+        #--------------------------------------------------
+        '1h' # Help/Information for 1
+        {
+# Write-Host ==> Pressed left for formatting; Leave on the left.
+Write-Host -ForegroundColor Yellow ("
+----------
+1) System\Domain Information
+----------
+Several options that gather basic information such as the Domain,  
+Domain Controller, and network tests related to the System and 
+Domain.") # END WRITE-HOST
+
+            Pause
+
+        } # End 1h-----------------------------------------
+        #--------------------------------------------------
+        # Option 2 - Current GPOs--------------------------
+        '2' 
+        {
+            Write-Host "2) "
+            If ($Global:quit -eq "quit") {break}
+
+
+        }#END option 2-----------------------------------------------
+        #--------------------------------------------------
+        '2h' # Help/Information for 2
+        {
+# Write-Host ==> Pressed left for formatting; Leave on the left.         
+Write-Host -ForegroundColor Yellow ("
+----------
+2) Information on 
+----------") # END WRITE-HOST
+
+            Pause
+        } # End 2h-----------------------------------------
+        #--------------------------------------------------
+        # Option 3 
+        '3' 
+        {
+
+            If ($Global:quit -eq "quit") {break}
+
+        }#END option 3------------------------------------- 
+        #--------------------------------------------------
+        '3h' # Help/Information for 3
+        {
+# Write-Host ==> Pressed left for formatting; Leave on the left.
+Write-Host -ForegroundColor Yellow ("
+----------
+3) 
+----------n") # END WRITE-HOST
+
+            Pause
+        } # End 3h-----------------------------------------
+        #--------------------------------------------------
+        # Option 4
+        '4' 
+        {
+            Write-Host ("")
+            If ($Global:quit -eq "quit") {break}
+
+        }#END option 4-------------------------------------   
+        #--------------------------------------------------
+        '4h' # Help/Information for 4
+        {
+# Write-Host ==> Pressed left for formatting; Leave on the left.
+Write-Host -ForegroundColor Yellow ("
+----------
+4) ") # END WRITE-HOST
+
+            Pause
+        } # End 4h   -----------------------------------------  
+        #--------------------------------------------------
+        # Option 5 - Create New GPOs-----------------------
+        '5' 
+        {
+            If ($Global:quit -eq "quit") {break}
+
+        }#END option 5-------------------------------------
+        #--------------------------------------------------
+        '5h' # Help/Information for 5
+        {
+# Write-Host ==> Pressed left for formatting; Leave on the left.
+Write-Host -ForegroundColor Yellow ("
+----------
+5) ") # END WRITE-HOST
+
+            Pause
+        } # End 5h-----------------------------------------
+        #--------------------------------------------------
+        # Option 6 
+        '6' 
+        {
+            If ($Global:quit -eq "quit") {break}
+
+        }#END option 6-------------------------------------
+        #--------------------------------------------------
+        '6h' # Help/Information for 6
+        {
+# Write-Host ==> Pressed left for formatting Leave on the left.
+Write-Host -ForegroundColor Yellow ("
+----------
+6) ") # END WRITE-HOST
+
+            Pause
+        } # End 6h-----------------------------------------
+        #--------------------------------------------------
+        # Option 7 
+        '7' 
+        {
+            If ($Global:quit -eq "quit") {break}
+
+        }#END option 7-------------------------------------
+        #--------------------------------------------------
+        '7h' # Help/Information for 7
+        {
+# Write-Host ==> Pressed left for formatting; Leave on the left.
+Write-Host -ForegroundColor Yellow ("
+----------
+7)  ") # END WRITE-HOST
+
+            Pause
+        } # End 7h-----------------------------------------
+        #--------------------------------------------------
+        # Option 8 - View all functions created for WindowsForensicsModule-----------------------
+        '8' 
+        {
+            # Assumes execution of .\RunWindowsForensicsProgram.ps1 was done from inside \GPOProgam
+            $functionPath = "C:\Program Files\WindowsPowerShell\Modules\WindowsForensicsModule"
+            $functionList = Get-ChildItem "$functionPath\Functions\" -Force -Name
+            do {
+              $pick, $choiceName = ""
+              Write-Host("`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n`n") # Terminal leaves residual text. This overwrites before clear
+              Clear-Host
+
+
+              do {
+                  $count=0
+                  Clear-Host
+                  Write-Host -ForegroundColor Yellow ("-------------`nWindowsForensicsModule Functions `n-------------`nThese functions are built to be used by the RunWindowsForensicsProgram, but nearly all can be used independantly. `nBy running this RunWindowsForensicsProgram, these functions have been imported into the WindowsPowerShell module/memory cache. `nThis section is to provide details on what actions are being taken, while in the RunWindowsForensicsProgram:`n-------------")
+                  ForEach ($function in $functionList) {
+                    $count++
+                    # TrimEnd() ends up trimming too much, we need to be very selective
+                    $function = $function.Substring(0, ($function).IndexOf(".ps1"))
+                    Write-Host("$count`) $function")
+                  } # End ForEach-------------------------------
+      
+                  
+                  Write-Host -ForegroundColor Yellow ("-------------`nEnter 0 to Return the Prior Menu `nPick which WindowsForensicsModule function to view the help-summary on:")
+                  $pick = Read-Host("`(1 - $count`)")
+                  If ($pick -eq 0) {break}
+              } until($pick -in 1..$count)
+              
+
+              If ($Global:quit -eq "quit") {break}
+              If ($pick -eq 0) {
+                Clear-Host
+                break
+
+              # Account for a single value list (not possible, but safety)
+              } ElseIf (($pick -eq 1) -AND ($count -eq 1)) {
+                $choiceName = $functionList
+                $choiceName = $choiceName.Substring(0, ($choiceName).IndexOf(".ps1"))
+
+              } ElseIf (($pick -in 1..$count) -AND ($count -gt 1)) {
+                $choiceName = $functionList[$pick - 1]
+                $choiceName = $choiceName.Substring(0, ($choiceName).IndexOf(".ps1"))
+              } # End If/ElseIf
+
+
+              Clear-Host
+              Get-WindowsForensicsSummary -function $choiceName
+              Pause
+          } until($pick -eq 0)
+            
+        }#END option 8-------------------------------------
+        #--------------------------------------------------
+        '8h' # Help/Information for 8
+        {
+# Write-Host ==> Pressed left for formatting; Leave on the left.
+Write-Host -ForegroundColor Yellow ("
+----------
+8) Get-Help WindowsForensicsModule Functions
+----------
+This section offers the ability to view the individual functions help sections. These 
+functions have been imported into the WindowsPowerShell cache, and outside of this 
+RunWindowsForensicsProgram, their help sections can be viewed with the standard PowerShell help:
+
+    > Get-Help <functionName> -Full
+    > Get-Help <functionName> -Synopsis
+    > Get-Help <functionName> -Examples
+    
+However, while using the RunWindowsForensicsProgram in real-time, these PowerShell Get-Help functions
+can overwrite, and simply be messy in the terminal. This section has just re-created the
+Get-Help sections for each WindowsForensicsModule function, which can then be individually
+searched for, and displayed while still using the RunWindowsForensicsProgram.
+    ") # END WRITE-HOST
+
+            Pause
+        } # End 8h-----------------------------------------
+        #--------------------------------------------------
+        'readme' # Help/Information for main
+        {
+# Write-Host ==> Pressed left for formatting; Leave on the left.
+Write-Host -ForegroundColor Yellow ("
+-------------------------------------------------
+README - WindowsForensicsModule and RunWindowsForensicsProgram Summary
+-------------------------------------------------
+
+  The 'WindowsForensicsModule' was built to accomplish extensic Windows Forensics gathering.
+
+    > Get-Help <function name> -full
+
+
+  Step1: 
+  Install the WindowsForensicsModule
+  Once downloaded, or cloned, then move to the downloaded `"WindowsForensicsModule`" directory.
+  From PowerShell:    
+  	  > .\InstallWindowsForensicsModule.ps1
+  
+
+  This is the installation file for the entire module.
+  Run this and the entire WindowsForensicsModule will be installed as a PowerShell module.
+  You can also right-click on `"InstallWindowsForensicsModule.ps1`" and click:
+    `"Run with Powershell`"
+
+
+  You only need to run this once, and it will be installed. 
+  Once installed, you can run functions individually or run the `"RunWindowsForensicsProgram`".
+
+
+
+  All functions associated were built to be run primarily from the 'RunWindowsForensicsProgram'.
+  Once `"InstallWindowsForensicsModule.ps1`" has been installed,
+  From the directory location:       .....\WindowsForensicsModule\RunWindowsForensicsProgram\
+  From PowerShell:
+      > .\RunWindowsForensicsProgram.ps1
+
+      Or you can right click on `"RunWindowsForensicsProgram.ps1`" and click `"Run with PowerShell`"
+
+
+  ALSO - once `"InstallWindowsForensicsModule.ps1`" has been run, and WindowsForensicsModule has been 
+  installed in the PowerShell cache, you can then copy the `"RunWindowsForensicsProgram - Shortcut`" anywhere
+  on the machine. For example, once `"WindowsForensicsModule`" has been installed, 
+      Right click on  `"RunWindowsForensicsProgram - Shortcut`"
+      Click:  `"Pin to Taskbar`"
+
+
+  Now if you click on this Taskbar icon, the WindowsForensicsModule will run anytime.
+  However, regardless of where it is executed from, the default location that 
+  the 'RunWindowsForensicsProgram' will always use for data, reports, backups etc., is the 
+  'WindowsPowerShell' location that the program copies itself to during execution:
+
+    'C:\Program Files\WindowsPowerShell\Modules\WindowsForensicsModule'
+
+
+
+  ------------------------------------------------------------------
+  This Module setup can be done automatically by running the 'InstallWindowsForensicsModule.ps1'.
+  As an Administrator in PowerShell:
+
+
+	1) Move to where the directory was downloaded to 'cd ....\WindowsForensicsModule\'
+	2) Once at the \WindowsForensicsModule location, from PowerShell Terminal run:
+
+	  > .\InstallWindowsForensicsModule.ps1
+
+  Or you can right click on `"InstallWindowsForensicsModule.ps1`" and click `"Run with PowerShell`"
+
+
+	3) This will automatically:
+		a) Copy the 'WindowsForensicsModule' into 'C:\Program Files\WindowsPowerShell\Modules'
+		b) Import the modules into the PowerShell memory cache.
+		c) This only needs to be done 1-time, when you first download WindowsForensicsModule
+
+
+
+  As stated, the 'WindowsForensicsModule' was built to be used through the 'RunWindowsForensicsProgram'; but 
+  most of these functions can be run individually IF they have been installed/imported into 
+  the PowerShell Module cache. This can be done in two ways:
+
+  1) Easiest is to follow the instructions above.
+
+
+  OR
+
+
+  2) Standard PowerShell Module Installation:
+  ------------------------------------------------------------------
+  2a) First action is to copy the 'WindowsForensicsModule' directory to:
+
+    'C:\Program Files\WindowsPowerShell\Modules'
+
+
+
+
+  2b) This can be done through PowerShell.
+      First, move TO the downloaded \WindowsForensicsModule location such as, 
+      \Desktop, \Downloads, etc., then copy the directory to the target:
+
+    > Copy-Item .\WindowsForensicsModule\ -Destination `"C:\Program Files\WindowsPowerShell\Modules`" -Force -Recurse
+
+
+
+
+  2c) Then from PowerShell, execute the target module import:
+
+    > Import-Module -Name `"WindowsForensicsModule`"
+
+
+
+
+  Now all 'WindowsForensicsModule' functions will be active.
+  It is still recommended to operate 'WindowsForensicsModule' through the:
+
+    > .\RunWindowsForensicsProgram.ps1
+
+
+
+
+
+
+  To Remove the WindowsForensicsModule:
+  ------------------------------------------------------------------
+  NOTE - There may be an error in PowerShell if you have this directory open 
+  in Windows Finder while you try to execute this Remove-Item:
+
+    > Remove-Item -Path `"C:\Program Files\WindowsPowerShell\Modules\WindowsForensicsModule\`" -Force -Recurse
+
+
+
+
+  Once you have removed the directory, then remove the module from the memory cache: 
+
+    > Remove-Module -Name `"WindowsForensicsModule`"
+ ") # END WRITE-HOST
+
+            Pause
+        } # End readme-----------------------------------------
+        #--------------------------------------------------
+        #------------------------  
+        'quit' 
+        {
+            $Global:quit = "quit"
+            $choice = "q"
+        } # End quit       
+    }# END Switch
+
+    Display-MainMenu #Refresh main menu
+
+} until (($choice -eq '0')) #END do Loop
